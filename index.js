994ec2d397385f4b5152bbf67149d5e6e1b4286d
Kristhel <klfpaz@scl.edu.gt>
2025-02-04 18:07:08 -0600

    Header trabajado

--- index.js
+++ index.js
@@ -1,12 +1,10 @@
-import {header} from "./componentes/header/header.js";
-import {formulario} from "./componentes/formulario/formulario.js"; 
-import {tareas} from "./componentes/tareas/tareas.js";
+import {headerTrabajo } from "./componentes/header/header.js";  
+import { formulario } from "./componentes/formulario/formulario.js"; 
+import { tareas } from "./componentes/tareas/tareas.js";
 
 let DOM = document.querySelector("#root");
 
 
-DOM.appendChild(header());
+DOM.appendChild(headerTrabajo());  
 DOM.appendChild(formulario());
 DOM.appendChild(tareas());
-
-
